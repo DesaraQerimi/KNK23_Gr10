24a3846a8b5572d4f57eca72480d3480cf081596
Enis Hoxha <enishoxha10@icloud.com>
2023-05-03 23:52:50 +0200

    Log In & Registration Form created

---
+++ demo/src/main/java/LoginRegistrationForm/form1controller.java
@@ -0,0 +1,13 @@
+package LoginRegistrationForm;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class form1controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+//        TODO
+    }
+}
