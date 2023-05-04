0faa29824e8afe5f39f524a4dd17c9d9b45e305a
Enis Hoxha <enishoxha10@icloud.com>
2023-05-04 02:50:16 +0200

    Fixed switch form

--- demo/src/main/java/LoginRegistrationForm/form1controller.java
+++ demo/src/main/java/LoginRegistrationForm/form1controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
@@ -51,17 +52,16 @@ public class form1controller implements Initializable {
     @FXML
     private TextField su_username;
 
-//    public void switchForm(ActionEvent event){
-//        if(event.getSource() == su_logintobtn){
-//            loginform.setVisible(true);
-//            signupform.setVisible(false);
-//        } else if(event.getSource() == li_createaccbtn){
-//            loginform.setVisible(false);
-//            signupform.setVisible(true);
-//        }
-//    }
-//    onAction="#switchForm" duhet me add te LogReg.fxml rreshti 19 & 49 (mas mnemonicParsing="false")
-//    nuk ka punu switch forma duhet me research pse
+    @FXML
+    public void switchForm(ActionEvent event){
+        if(event.getSource() == su_logintobtn){
+            loginform.setVisible(true);
+            signupform.setVisible(false);
+        } else if(event.getSource() == li_createaccbtn){
+            loginform.setVisible(false);
+            signupform.setVisible(true);
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
