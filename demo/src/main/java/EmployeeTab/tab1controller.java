4952b7164edcc2f1581c435ffa29e58457ff4576
Enis Hoxha <enishoxha10@icloud.com>
2023-05-05 08:14:18 +0200

    Testing shortcuts

--- demo/src/main/java/EmployeeTab/tab1controller.java
+++ demo/src/main/java/EmployeeTab/tab1controller.java
@@ -1,15 +1,11 @@
 package EmployeeTab;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -32,9 +28,24 @@ public class tab1controller implements Initializable {
     @FXML
     private AnchorPane punetoretPage;
 
+    @FXML
+    private MenuItem closeItem;
+
+    @FXML
+    private MenuBar menuBar;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 //        TODO
     }
+
+    @FXML
+    private void closeWindow(ActionEvent event) {
+        if (event.getSource() == closeItem) {
+            Stage stage = (Stage) menuBar.getScene().getWindow();
+            stage.close();
+        }
+    }
+
 }
