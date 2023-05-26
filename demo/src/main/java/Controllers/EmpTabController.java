af7c03f05d5aac80bf804c7c41d09f5ff0538e4f
dearexha <dea.rexha01@gmail.com>
2023-05-26 12:51:05 +0200

    retrieve-employees

--- demo/src/main/java/Controllers/EmpTabController.java
+++ demo/src/main/java/Controllers/EmpTabController.java
@@ -1,5 +1,11 @@
 package Controllers;
 
+import Services.Employee;
+import Services.EmployeeService;
+import Services.GradaDetails;
+import Services.RoliDetails;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,14 +14,21 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import Services.ConnectionUtil;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EmpTabController implements Initializable {
     @FXML
@@ -48,18 +61,59 @@ public class EmpTabController implements Initializable {
     @FXML
     private MenuBar menuBar;
 
+    @FXML
+    private TableColumn<EmployeeService, String> colName;
+
+    @FXML
+    private TableColumn<EmployeeService, String> colSurname;
+
+    @FXML
+    private TableColumn<EmployeeService, String> colDept;
+
+    @FXML
+    private TableColumn<EmployeeService, String> colEmail;
+
+    @FXML
+    private TableColumn<EmployeeService, String> colPhone;
+
+    @FXML
+    private TableView<EmployeeService> tableEmp;
+
+    private ObservableList<EmployeeService> data;
+
+    private Connection db;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        TODO
+        try {
+            db = ConnectionUtil.getConnection();
+            setupTableColumns();
+            loadDataFromDatabase(); // Load data from the database into the table
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private void setupTableColumns() {
+        colName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        colSurname.setCellValueFactory(new PropertyValueFactory<>("lastname"));
+        colDept.setCellValueFactory(new PropertyValueFactory<>("department"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+    }
+
+    private void loadDataFromDatabase() throws SQLException {
+        data = EmployeeService.getAllEmployees();
+        tableEmp.setItems(data);
+    }
+
 
     public void changeWindow(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("Salaries.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
 
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
         window.show();
     }
@@ -80,6 +134,7 @@ public class EmpTabController implements Initializable {
     }
 
     Stage stage;
+
     public void logOut(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setHeaderText("You are about to log out!");
@@ -104,3 +159,7 @@ public class EmpTabController implements Initializable {
         }
     }
 }
+
+
+
+
--- demo/src/main/java/Repositories/EmployeeRepository.java
+++ demo/src/main/java/Repositories/EmployeeRepository.java
@@ -3,9 +3,7 @@ package Repositories;
 import Models.Roli;
 import Services.ConnectionUtil;
 import Models.Employee;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,110 +11,119 @@ import java.util.List;
 
 public class EmployeeRepository {
 
+
     private static Connection conn;
+
+    static {
+        try {
+            conn = ConnectionUtil.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public EmployeeRepository() throws SQLException {
         conn = ConnectionUtil.getConnection();
     }
 
-
     public static List<Employee> getAllEmployees() throws SQLException {
         List<Employee> employees = new ArrayList<>();
-        PreparedStatement stmt = EmployeeRepository.conn.prepareStatement("SELECT * FROM employees");
-
-        ResultSet rs = stmt.executeQuery();
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String firstName = rs.getString("name");
-            String lastName = rs.getString("lastname");
-            String department = rs.getString("department");
-            String email = rs.getString("email");
-            String phone = rs.getString("phone");
-            Roli roli = (Roli) rs.getObject("roli"); //TODO
-            Date start_date = rs.getDate("start_date");
-            Date end_date = rs.getDate("end_date");
-            Employee employee = new Employee(firstName, lastName, department, email, phone);
-            employees.add(employee);
+        String sql = "SELECT * FROM employees";
+        try (Statement statement = conn.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String firstName = rs.getString("name");
+                String lastName = rs.getString("lastname");
+                String department = rs.getString("department");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                Employee employee = new Employee(id, firstName, lastName, department, email, phone);
+                employees.add(employee);
+            }
         }
         return employees;
     }
 
     public static Employee addEmployee(Employee employee) throws SQLException {
         String sql = "INSERT INTO employees (name, lastname, department, email, phone) VALUES (?, ?, ?, ?, ?)";
-        Connection connection = ConnectionUtil.getConnection();
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1, employee.getFirstName());
-        statement.setString(2, employee.getLastName());
-        statement.setString(3, employee.getDepartment());
-        statement.setString(4, employee.getEmail());
-        statement.setString(5, employee.getPhone());
-        statement.executeUpdate();
+        try (PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, employee.getFirstName());
+            statement.setString(2, employee.getLastName());
+            statement.setString(3, employee.getDepartment());
+            statement.setString(4, employee.getEmail());
+            statement.setString(5, employee.getPhone());
+            statement.executeUpdate();
 
-        return EmployeeRepository.getByName(employee.getFirstName());
-    }
-
-    public void updateEmployee(Employee employee) throws SQLException {
-        PreparedStatement stmt = this.conn.prepareStatement("UPDATE employees SET name = ?, lastname = ?, department = ?, email = ?, phone = ? WHERE id = ?");
-        stmt.setString(1, employee.getFirstName());
-        stmt.setString(2, employee.getLastName());
-        stmt.setString(3, employee.getDepartment());
-        stmt.setString(4, employee.getEmail());
-        stmt.setString(5, employee.getPhone());
-        stmt.setInt(8, employee.getId());
-        stmt.executeUpdate();
-    }
-
-    public void deleteEmployee(int id) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("DELETE FROM employees WHERE id = ?");
-        stmt.setInt(1, id);
-        stmt.executeUpdate();
-    }
-
-    public static Employee getByName(String name) throws SQLException{
-        String sql = "SELECT * FROM employees WHERE name = ?";
-
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, name);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String lastname = resultSet.getString("lastname");
-                String department = resultSet.getString("department");
-                String email = resultSet.getString("email");
-                String phone = resultSet.getString("phone");
-
-                return new Employee(id, name, lastname, department, email, phone);
-            } else {
-                return null;
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    employee.setId(generatedId);
+                    return employee;
+                } else {
+                    throw new SQLException("Failed to add employee, no ID obtained.");
+                }
             }
         }
     }
 
-    public static List<Employee> getAllEmployees(String string) throws SQLException {
-        List<Employee> employees = new ArrayList<>();
-        String sql = "SELECT * FROM employees";
-        Connection conn = ConnectionUtil.getConnection();
-        ResultSet rs = conn.createStatement().executeQuery(sql);
-
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String lastname = rs.getString("lastname");
-            int roli = rs.getInt("roli");
-            Date start_date = rs.getDate("start_date");
-            Date end_date = rs.getDate("end_date");
-            String department = rs.getString("department");
-            String email = rs.getString("email");
-            String phone = rs.getString("phone");
-            Employee employee = new Employee(id, name, lastname, department, email, phone);
-
-            // TODO: fix roli
-            employees.add(employee);
+    public void updateEmployee(Employee employee) throws SQLException {
+        String sql = "UPDATE employees SET name = ?, lastname = ?, department = ?, email = ?, phone = ? WHERE id = ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, employee.getFirstName());
+            statement.setString(2, employee.getLastName());
+            statement.setString(3, employee.getDepartment());
+            statement.setString(4, employee.getEmail());
+            statement.setString(5, employee.getPhone());
+            statement.setInt(6, employee.getId());
+            statement.executeUpdate();
         }
-
-        return employees;
     }
 
-}
+    public void deleteEmployee(int id) throws SQLException {
+        String sql = "DELETE FROM employees WHERE id = ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        }
+    }
 
+    public Employee getByName(String name) throws SQLException {
+        String sql = "SELECT * FROM employees WHERE name = ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, name);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String lastName = resultSet.getString("lastname");
+                    String department = resultSet.getString("department");
+                    String email = resultSet.getString("email");
+                    String phone = resultSet.getString("phone");
+                    return new Employee(id, name, lastName, department, email, phone);
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
+
+    public List<Employee> getAllEmployees(String string) throws SQLException {
+        List<Employee> employees = new ArrayList<>();
+        String sql = "SELECT * FROM employees";
+        try (Statement statement = conn.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String lastName = rs.getString("lastname");
+                String department = rs.getString("department");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                Employee employee = new Employee(id, name, lastName, department, email, phone);
+                employees.add(employee);
+            }
+        }
+        return employees;
+    }
+}
