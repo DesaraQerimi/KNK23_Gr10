3fe8c1d32a28bf3ba0964f84890e4807e8403aa1
dearexha <dea.rexha01@gmail.com>
2023-05-27 22:27:04 +0200

    on-enter-close

--- demo/src/main/java/Controllers/AddNewFormController.java
+++ demo/src/main/java/Controllers/AddNewFormController.java
@@ -9,6 +9,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -38,26 +40,43 @@ public class AddNewFormController implements Initializable {
     private Button handelAddBtn;
 
     Alert alert = new Alert(Alert.AlertType.ERROR,"");
-    public void add(ActionEvent ae) throws Exception{
-        if(!addnewName.getText().isBlank() && !addnewSurname.getText().isBlank() &&
+
+    public void add(ActionEvent ae) throws Exception {
+        if (!addnewName.getText().isBlank() && !addnewSurname.getText().isBlank() &&
                 !addnewDepartment.getText().isBlank() && !addnewEmail.getText().isBlank() &&
-                !addnewPhone.getText().isBlank()){
+                !addnewPhone.getText().isBlank()) {
 
             Employee employee = new Employee(addnewName.getText(), addnewSurname.getText(),
                     addnewDepartment.getText(), addnewEmail.getText(), addnewPhone.getText());
 
             EmployeeService.addEmployee(employee);
 
-        }else{
+            // Close the window
+            addNew.getScene().getWindow().hide();
+        } else {
             alert.setContentText("Fill all the fields!");
             alert.show();
         }
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        TODO
+        // Register event handler for Enter key press
+        addnewName.setOnKeyPressed(this::handleEnterKeyPress);
+        addnewSurname.setOnKeyPressed(this::handleEnterKeyPress);
+        addnewDepartment.setOnKeyPressed(this::handleEnterKeyPress);
+        addnewEmail.setOnKeyPressed(this::handleEnterKeyPress);
+        addnewPhone.setOnKeyPressed(this::handleEnterKeyPress);
+    }
+
+    private void handleEnterKeyPress(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            try {
+                add(new ActionEvent());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
