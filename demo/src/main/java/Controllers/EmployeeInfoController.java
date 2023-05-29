2c1668f6d71317acec70ee8b46214abd0b84058a
Enis Hoxha <enishoxha10@icloud.com>
2023-05-29 08:32:03 +0200

    update-version

--- demo/src/main/java/Controllers/EmployeeInfoController.java
+++ demo/src/main/java/Controllers/EmployeeInfoController.java
@@ -51,21 +51,66 @@ public class EmployeeInfoController implements Initializable {
 
     private File selectedContractFile;
 
-//    @FXML
-//    private void uploadContract(ActionEvent event) {
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle("Select Contract PDF File");
-//        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
-//
-//        // Show the file chooser dialog
-//        File selectedFile = fileChooser.showOpenDialog(empUpload.getScene().getWindow());
-//
-//        if (selectedFile != null) {
-//            // File selected, store the reference
-//            selectedContractFile = selectedFile;
-//        }
-//    }
+    private String email;
 
+    @FXML
+    private void initializeContractStatusLabel() {
+        try {
+            // Retrieve the employee's contract value from the database using their email
+            Connection conn = ConnectionUtil.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("SELECT contract FROM employees WHERE email = ?");
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                // Get the contract value from the result set
+                String contractValue = rs.getString("contract");
+
+                if (contractValue != null && !contractValue.isEmpty()) {
+                    // If the contract value is not empty, set the label text
+                    contractStatusLabel.setText("Ky employee ka kontratë pune");
+                }
+            }
+
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @FXML
+    private void uploadContract(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Contract PDF File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+
+        // Show the file chooser dialog
+        File selectedFile = fileChooser.showOpenDialog(empUpload.getScene().getWindow());
+
+        if (selectedFile != null) {
+            // File selected, store the reference
+            selectedContractFile = selectedFile;
+
+            // Get the file name
+            String fileName = selectedContractFile.getName();
+
+            // Update the contract column in the database with the file name
+            try {
+                Connection conn = ConnectionUtil.getConnection();
+                PreparedStatement stmt = conn.prepareStatement("UPDATE employees SET contract = ? WHERE email = ?");
+                stmt.setString(1, fileName);
+                stmt.setString(2, email);
+                stmt.executeUpdate();
+
+                // Update the label text
+                contractStatusLabel.setText("");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public void setEmployee(EmployeeService employee) {
         this.employeeService = employee;
 
@@ -75,6 +120,10 @@ public class EmployeeInfoController implements Initializable {
         empDep.setText(employee.getDepartment());
         empEmail.setText(employee.getEmail());
         empPhone.setText(employee.getPhone());
+
+        email = empEmail.getText();
+
+        initializeContractStatusLabel();
     }
 
 
@@ -88,10 +137,13 @@ public class EmployeeInfoController implements Initializable {
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.OK) {
             try {
-                // Delete the employee from the database
+                // Get the employee ID from EmployeeService
+                int employeeId = Employee.getId();
+
+                // Delete the employee from the database using the employee ID
                 Connection conn = ConnectionUtil.getConnection();
-                PreparedStatement stmt = conn.prepareStatement("DELETE FROM employees WHERE id = ?");
-                stmt.setInt(1, Employee.getId());
+                PreparedStatement stmt = conn.prepareStatement("DELETE FROM employees WHERE email = ?");
+                stmt.setString(1, email);
                 stmt.executeUpdate();
 
                 // Close the current window
@@ -101,6 +153,7 @@ public class EmployeeInfoController implements Initializable {
             }
         }
     }
+
 
     @FXML
     private void updateEmployee(ActionEvent event) {
